6af0c79a8ae0e275b7bfec9c2a4d8c02042f8096
Hudeyfa <hudayfa@hotmail.com>
2024-10-09 18:11:49 +0200

    næsten færdig

--- credit-card-client/src/app/services/services/credit-card.service.ts
+++ credit-card-client/src/app/services/services/credit-card.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +28,22 @@ export class CreditCardService {
   // Add a new Card
   createCreditCard(cardData: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/cards`, cardData);
+  }
+
+  // Get all cards
+  getCreditCards(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/cards`);
+  }
+
+  // Delete a card using the card number
+  deleteCreditCard(card_number: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/cards/${card_number}`);
+  }
+
+  // Get a card by card number by filtering all cards
+  getCardByNumber(card_number: string): Observable<any> {
+    return this.getCreditCards().pipe(
+      map(cards => cards.find(card => card.card_number === card_number))
+    );
   }
 }
